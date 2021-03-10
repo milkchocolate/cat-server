0c52f2f439c5bfedf884de81c6623d127bbb7783
Chocolate Yu <aaron@live.com>
2021-03-10 20:37:29 +0800

    Add counter

--- src/main/java/org/cat/catserver/CatController.java
+++ src/main/java/org/cat/catserver/CatController.java
@@ -11,10 +11,18 @@ import java.util.Map;
 @RestController
 public class CatController {
     private final List<Cat> cats = new ArrayList<>();
+    private int counter = 0;
 
-    public CatController(){
+    public CatController() {
         cats.add(new Cat("white"));
         cats.add(new Cat("black"));
+    }
+
+    @GetMapping("/counter")
+    public int getCounter() {
+        counter++;
+        System.out.println(counter);
+        return counter;
     }
 
     @GetMapping("/cats")
