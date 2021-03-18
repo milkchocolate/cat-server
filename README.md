7cb15a665928635ba70fc69f06c89491a3c312e6
Chocolate Yu <aaron@live.com>
2021-03-18 19:59:21 +0800

    Add items endpoint

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cat-server
 ![build workflow](https://github.com/milkchocolate/cat-server/actions/workflows/build.yml/badge.svg)
+
+## Call /items endpoint
+```sh
+curl http://localhost:8080/cat-server/items?names=HOSTNAME&delay=0&statusCode=200
+```
--- src/main/java/org/cat/catserver/CatController.java
+++ src/main/java/org/cat/catserver/CatController.java
@@ -1,12 +1,15 @@
 package org.cat.catserver;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class CatController {
@@ -16,6 +19,25 @@ public class CatController {
     public CatController() {
         cats.add(new Cat("white"));
         cats.add(new Cat("black"));
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<String>> getItems(
+            @RequestParam String names,
+            @RequestParam int delay,
+            @RequestParam int statusCode) throws InterruptedException {
+        counter++;
+        System.out.println(counter);
+        if (delay > 0) {
+            Thread.sleep(delay * 1000L);
+        }
+        if (statusCode <= 0) {
+            statusCode = 200;
+        }
+        var values = Arrays.stream(names.split(","))
+                .map(System::getenv)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(values, HttpStatus.valueOf(statusCode));
     }
 
     @GetMapping("/counter")
